06b55e8e977c300cd0b1d36e0232fbd64a8678d8
roy-stephanie <korettex@gmail.com>
2022-11-17 13:43:23 +0100

    Refactoring Component Logo

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -1,10 +1,11 @@
 import "./Footer.scss";
-import LogoFooter from "../logo-footer/LogoFooter";
+import Logo from "../logo/Logo";
+import logoFooter from "./img/Logo-White.png";
 
 export default function Footer() {
   return (
     <footer>
-      <LogoFooter src="/img/Logo-White.png" />
+      <Logo src={logoFooter} />
       <p>© 2020 Kasa. All rights reserved</p>
     </footer>
   );
--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,12 +1,13 @@
 import { NavLink } from "react-router-dom";
 import Logo from "../logo/Logo";
+import logoHeader from "./img/Logo-Primary.png";
 import "./Header.scss";
 
 export default function Header() {
   return (
     <header>
       <nav className="navbar">
-        <Logo src="/img/Logo-Primary.png" />
+        <Logo src={logoHeader} />
         <ul className="navbar_menu">
           <li><NavLink to="/" end>Accueil</NavLink></li>
           <li><NavLink to="/about">A Propos</NavLink></li>
